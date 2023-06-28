971882a7ba5ea8137ed7ce9ad3f125dcda1dfdb8
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-06-28 12:08:55 +0800

    login function half done

--- internal/login/login.go
+++ internal/login/login.go
@@ -3,10 +3,51 @@ package login
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	_ "crypto/md5"
 )
 
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Token    string `json:"token"`
+}
+
+type LoginResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+	User    User   `json:"user"`
+}
+
 func Init(c *gin.Context) {
-	//msg := c.PostForm("input")
-	msg := c.DefaultPostForm("input", "表單沒有input。") // 沒有輸入參數時 可設定預設值
-	c.String(http.StatusOK, "您輸入的文字為: \n%s", msg)
+
+	// Receive request
+	var req LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	
+
+	// Create response
+	res := LoginResponse{
+		Success: true,
+		Message: "登入成功",
+		Code:    200,
+		User: User{
+			ID:       123,
+			Username: req.Username,
+			Email:    "john_doe@example.com",
+			Token:    "anyScriptyouchooseorLoremLorem",
+		},
+	}
+
+	c.JSON(http.StatusOK, res)
 }
