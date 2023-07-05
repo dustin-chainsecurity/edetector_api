bc095cabb45830506fa68d2017ead5a4b51e318b
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-07-05 16:38:36 +0800

    update changedetectmode function

--- internal/api/task/changeDetectMode.go
+++ internal/api/task/changeDetectMode.go
@@ -2,7 +2,10 @@ package task
 
 import (
 	"edetector_API/pkg/logger"
+	"edetector_API/pkg/mariadb"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +15,43 @@ func ChangeDetectMode(c *gin.Context) {
 	var req TaskRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return
+	}
+
+	// fetch agent current setting
+	var process, network int
+	query := "SELECT processreport, networkreport FROM client_setting WHERE client_id = ?"
+	err := mariadb.DB.QueryRow(query, req.Key).Scan(&process, &network)
+	if err != nil {
+		logger.Error("Error retrieving current client setting: " + err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"Error retrieving current client setting": err.Error()})
+		return
+	}
+
+	// check if the changes are needed
+	values := strings.Split(req.Message, "|")  // "0|0"
+	// Convert string values to integers
+	new_process, err1 := strconv.Atoi(values[0])
+	new_network, err2 := strconv.Atoi(values[1])
+	if err1 != nil || err2 != nil {
+		logger.Error("Invalid message input: " + err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"Invalid message input": err.Error()})
+		return
+	}
+	if new_process == process && new_network == network {
+		res := TaskResponse {
+			IsSuccess: true,
+			Message: "Nothing changed",
+		}
+		c.JSON(http.StatusOK, res)
+	}
+
+	// update agent settings
+	query = "UPDATE user_info SET processreport = ?, networkreport = ? WHERE id = ?"
+	_, err = mariadb.DB.Exec(query, new_process, new_network)
+	if err != nil {
+		logger.Error("Error updating client setting: " + err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"Error updating client setting": err.Error()})
 		return
 	}
 
--- internal/api/task/packet.go
+++ internal/api/task/packet.go
@@ -11,14 +11,14 @@ import (
 type TaskRequest struct {
 	Key      string      `json:"key"`
 	User     string      `json:"user"`
-	Message  interface{} `json:"message"`
+	Message  string `json:"message"`
 }
 
 type TaskPacket struct {
 	Key      string      `json:"key"`
 	Work     string      `json:"work"`
 	User     string      `json:"user"`
-	Message  interface{} `json:"message"`
+	Message  string `json:"message"`
 }
 
 type TaskResponse struct {
@@ -32,14 +32,14 @@ func SendTCPRequest(c *gin.Context, work string, req TaskRequest) (*TaskResponse
 	conn, ok := c.Get("tcpConnection")
 	if !ok {
 		// TCP connection not found in context
-		return nil, fmt.Errorf("Failed to get TCP connection")
+		return nil, fmt.Errorf("failed to get TCP connection")
 	}
 
 	// Type assertion to convert the connection to the appropriate type
 	tcpConn, ok := conn.(net.Conn)
 	if !ok {
 		// Invalid TCP connection type
-		return nil, fmt.Errorf("Invalid TCP connection")
+		return nil, fmt.Errorf("invalid TCP connection")
 	}
 
 	// Generate server request
@@ -53,27 +53,27 @@ func SendTCPRequest(c *gin.Context, work string, req TaskRequest) (*TaskResponse
 	// Convert to JSON
 	reqJSON, err := json.Marshal(ServerReq)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to convert req to JSON: %s", err.Error())
+		return nil, fmt.Errorf("failed to convert req to JSON: %s", err.Error())
 	}
 
 	// Send the JSON request
 	_, err = tcpConn.Write(reqJSON)
 	if err != nil {
-		return nil, fmt.Errorf("Error sending request to working server: %s", err.Error())
+		return nil, fmt.Errorf("error sending request to working server: %s", err.Error())
 	}
 
 	// Receive the response
 	buf := make([]byte, 1024)
 	reslen, err := tcpConn.Read(buf)
 	if err != nil {
-		return nil, fmt.Errorf("Error receiving response: %s", err.Error())
+		return nil, fmt.Errorf("error receiving response: %s", err.Error())
 	}
 
 	// Parse the JSON response
 	var response TaskResponse
 	err = json.Unmarshal(buf[:reslen], &response)
 	if err != nil {
-		return nil, fmt.Errorf("Error parsing JSON response: %s", err.Error())
+		return nil, fmt.Errorf("error parsing JSON response: %s", err.Error())
 	}
 
 	return &response, nil
