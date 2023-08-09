446e217cc4c7b750aedd16c5909d68b0ec6da751
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-08-09 18:38:27 +0800

    finished group-related apis

--- api/group/group.go
+++ api/group/group.go
@@ -11,34 +11,58 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type addRequest struct {
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	RangeBegin  string  `json:"rangeBegin"`
-	RangeEnd	string  `json:"rangeEnd"`
-}
-
-type addResponse struct {
-	IsSuccess bool `json:"isSuccess"`
-	GroupID   int  `json:"groupID"`
-}
-
-type groupInfoResponse struct {
-	IsSuccess   bool   `json:"isSuccess"`
-	GroupID     int    `json:"groupID"`
+type updateGroupRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	RangeBegin  string `json:"rangeBegin"`
 	RangeEnd    string `json:"rangeEnd"`
 }
 
+type addGroupResponse struct {
+	IsSuccess bool `json:"isSuccess"`
+	GroupID   int  `json:"groupID"`
+}
+
+type groupInfoResponse struct {
+	IsSuccess   bool     `json:"isSuccess"`
+	GroupID     int      `json:"groupID"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	RangeBegin  string   `json:"rangeBegin"`
+	RangeEnd    string   `json:"rangeEnd"`
+	Devices     []string `json:"devices"`
+}
+
+type updateDevicesRequest struct {
+	Groups  []int      `json:"groups"`
+	Devices []string   `json:"devices"`
+}
+
+type group struct {
+	ID          int      `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	RangeBegin  string   `json:"rangeBegin"`
+	RangeEnd    string   `json:"rangeEnd"`
+	Devices     []string `json:"devices"`
+}
+
 func Add(c *gin.Context) {
-	req := addRequest{}
+	req := updateGroupRequest{}
 	if err := c.ShouldBindJSON(&req); err != nil {
 		errhandler.Handler(c, err, "Invalid request format")
 		return
 	}
 	logger.Info("Request content: " + fmt.Sprintf("%+v", req))
+
+	if exist, err := query.CheckGroupName(req.Name); err != nil {
+		errhandler.Handler(c, err, "Error checking group name existence")
+		return
+	} else if exist {
+		errhandler.Handler(c, fmt.Errorf("group name " + req.Name + " already exists"), "Error checking group name")
+		return
+	}
+
 	id, err := query.AddGroup(query.GroupInfo{
 		Name:        req.Name,
 		Description: req.Description,
@@ -49,17 +73,61 @@ func Add(c *gin.Context) {
 		errhandler.Handler(c, err, "Error adding group")
 		return
 	}
-	res := addResponse{
+	res := addGroupResponse{
 		IsSuccess: true,
 		GroupID:   id,
 	}
 	c.JSON(http.StatusOK, res)
 }
 
-func Remove(c *gin.Context) {
-	id, err :=  strconv.Atoi(c.Query("id"))
+func Update(c *gin.Context) {
+	req := updateGroupRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		errhandler.Handler(c, err, "Invalid request format")
+		return
+	}
+	logger.Info("Request content: " + fmt.Sprintf("%+v", req))
+	id, err := strconv.Atoi(c.Query("id"))
 	if err != nil {
-		errhandler.Handler(c, err, "Error converting group id")
+		errhandler.Handler(c, err, "Error retrieving groupID")
+		return
+	}
+	// check if the group exists
+	if _, err := query.CheckGroupID(id); err != nil {
+		errhandler.Handler(c, err, "Error checking groupID")
+		return
+	}
+	// check if the group name exists
+	if err := query.CheckGroupNameForUpdate(id, req.Name); err != nil {
+		errhandler.Handler(c, err, "Error checking group name")
+		return
+	}
+	err = query.UpdateGroup(query.GroupInfo{
+		ID:          id,
+		Name:        req.Name,
+		Description: req.Description,
+		RangeBegin:  req.RangeBegin,
+		RangeEnd:    req.RangeEnd,
+	})
+	if err != nil {
+		errhandler.Handler(c, err, "Error updating group")
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"isSuccess": true,
+	})
+}
+
+func Remove(c *gin.Context) {
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		errhandler.Handler(c, err, "Error retrieving groupID")
+		return
+	}
+	// check if the group exists
+	if _, err := query.CheckGroupID(id); err != nil {
+		errhandler.Handler(c, err, "Error checking groupID")
+		return
 	}
 	err = query.RemoveGroup(id)
 	if err != nil {
@@ -71,10 +139,27 @@ func Remove(c *gin.Context) {
 	})
 }
 
+func GetList(c *gin.Context) {
+	groups, err := query.LoadAllGroupInfo()
+	if err != nil {
+		errhandler.Handler(c, err, "Error loading group list")
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"isSuccess": true,
+		"groups":    groups,
+	})
+}
+
 func GetInfo(c *gin.Context) {
 	id, err := strconv.Atoi(c.Query("id"))
 	if err != nil {
-		errhandler.Handler(c, err, "Error converting group id")
+		errhandler.Handler(c, err, "Error retrieving groupID")
+		return
+	}
+	// check if the group exists
+	if _, err := query.CheckGroupID(id); err != nil {
+		errhandler.Handler(c, err, "Error checking groupID")
 		return
 	}
 	info, err := query.LoadGroupInfo(id)
@@ -84,19 +169,71 @@ func GetInfo(c *gin.Context) {
 	}
 	res := groupInfoResponse{
 		IsSuccess:   true,
-		GroupID:     id,
+		GroupID:     info.ID,
 		Name:        info.Name,
 		Description: info.Description,
 		RangeBegin:  info.RangeBegin,
 		RangeEnd:    info.RangeEnd,
+		Devices:     info.Devices,
 	}
 	c.JSON(http.StatusOK, res)
 }
 
 func Join(c *gin.Context) {
-	
+	req := updateDevicesRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		errhandler.Handler(c, err, "Invalid request format")
+		return
+	}
+	logger.Info("Request content: " + fmt.Sprintf("%+v", req))
+	// check if the all group exists
+	err := query.CheckAllGroupID(req.Groups)
+	if err != nil {
+		errhandler.Handler(c, err, "Error checking groupID")
+		return
+	}
+	// check if the all device exists
+	err = query.CheckAllDevice(req.Devices)
+	if err != nil {
+		errhandler.Handler(c, err, "Error checking deviceID")
+		return
+	}
+	err = query.AddGroupDevices(req.Groups, req.Devices)
+	if err != nil {
+		errhandler.Handler(c, err, "Error adding devices to group")
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"isSuccess": true,
+	})
 }
 
 func Leave(c *gin.Context) {
-	
+	// Remove devices from group
+	req := updateDevicesRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		errhandler.Handler(c, err, "Invalid request format")
+		return
+	}
+	logger.Info("Request content: " + fmt.Sprintf("%+v", req))
+	// check if the all group exists
+	err := query.CheckAllGroupID(req.Groups)
+	if err != nil {
+		errhandler.Handler(c, err, "Error checking groupID")
+		return
+	}
+	// check if the all device exists
+	err = query.CheckAllDevice(req.Devices)
+	if err != nil {
+		errhandler.Handler(c, err, "Error checking deviceID")
+		return
+	}
+	err = query.RemoveGroupDevices(req.Groups, req.Devices)
+	if err != nil {
+		errhandler.Handler(c, err, "Error removing devices from group")
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"isSuccess": true,
+	})
 }
--- pkg/mariadb/query/group.go
+++ pkg/mariadb/query/group.go
@@ -2,13 +2,17 @@ package query
 
 import (
 	"edetector_API/pkg/mariadb"
+	"fmt"
+	"strconv"
 )
 
 type GroupInfo struct {
-	Name        string
-	Description string
-	RangeBegin  string
-	RangeEnd	string
+	ID          int      `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	RangeBegin  string   `json:"rangeBegin"`
+	RangeEnd    string   `json:"rangeEnd"`
+	Devices     []string `json:"devices"`
 }
 
 func AddGroup(g GroupInfo) (int, error) {
@@ -29,6 +33,19 @@ func AddGroup(g GroupInfo) (int, error) {
 		return -1, err
 	}
 	return groupID, nil
+}
+
+func UpdateGroup(g GroupInfo) error {
+	query := `
+		UPDATE group_info
+		SET group_name = ?, description = ?, range_begin = ?, range_end = ?
+		WHERE group_id = ?;
+	`
+	_, err := mariadb.DB.Exec(query, g.Name, g.Description, g.RangeBegin, g.RangeEnd, g.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func RemoveGroup(groupID int) error {
@@ -53,7 +70,61 @@ func LoadGroupInfo(groupID int) (GroupInfo, error) {
 	if err != nil {
 		return GroupInfo{}, err
 	}
+	g.ID = groupID
+	g.Devices, err = LoadGroupDevices(groupID)
+	if err != nil {
+		return GroupInfo{}, err
+	}
 	return g, nil
+}
+
+func LoadAllGroupInfo() ([]GroupInfo, error) {
+	query := `
+		SELECT group_id, group_name, description, range_begin, range_end
+		FROM group_info;
+	`
+	rows, err := mariadb.DB.Query(query)
+	if err != nil {
+		return []GroupInfo{}, err
+	}
+	defer rows.Close()
+
+	groups := []GroupInfo{}
+	for rows.Next() {
+		var g GroupInfo
+		err := rows.Scan(&g.ID, &g.Name, &g.Description, &g.RangeBegin, &g.RangeEnd)
+		if err != nil {
+			return []GroupInfo{}, err
+		}
+		g.Devices, err = LoadGroupDevices(g.ID)
+		if err != nil {
+			return []GroupInfo{}, err
+		}
+		groups = append(groups, g)
+	}
+	return groups, nil
+}
+
+func LoadGroupDevices(groupID int) ([]string, error) {
+	query := `
+		SELECT client_id FROM client_group WHERE group_id = ?;
+	`
+	rows, err := mariadb.DB.Query(query, groupID)
+	if err != nil {
+		return []string{}, err
+	}
+	defer rows.Close()
+
+	devices := []string{}
+	for rows.Next() {
+		var device string
+		err := rows.Scan(&device)
+		if err != nil {
+			return []string{}, err
+		}
+		devices = append(devices, device)
+	}
+	return devices, nil
 }
 
 func LoadGroups(deviceId string) ([]string, error) {
@@ -68,15 +139,118 @@ func LoadGroups(deviceId string) ([]string, error) {
 		return []string{}, err
 	}
 	defer rows.Close()
-	
+
 	groups := []string{}
 	for rows.Next() {
 		var groupName string
 		err := rows.Scan(&groupName)
 		if err != nil {
 			return []string{}, err
-        }
+		}
 		groups = append(groups, groupName)
 	}
 	return groups, nil
+}
+
+func AddGroupDevices(groups []int, devices []string) error {
+	query := `
+		INSERT INTO client_group (group_id, client_id)
+		VALUES (?, ?);
+	`
+	for _, group := range groups {
+		for _, device := range devices {
+			if exist, err := InGroup(group, device); err != nil {
+				return err
+			} else if exist {
+				continue
+			}
+			_, err := mariadb.DB.Exec(query, group, device)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func RemoveGroupDevices(groups []int, devices []string) error {
+	query := `
+		DELETE FROM client_group
+		WHERE group_id = ? AND client_id = ?;
+	`
+	for _, group := range groups {
+		for _, device := range devices {
+			if exist, err := InGroup(group, device); err != nil {
+				return err
+			} else if exist {
+				_, err := mariadb.DB.Exec(query, group, device)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func CheckGroupID(groupID int) (bool, error) {
+	query := `
+		SELECT EXISTS(SELECT group_id FROM group_info WHERE group_id = ?);
+	`
+	var exist bool
+	err := mariadb.DB.QueryRow(query, groupID).Scan(&exist)
+	if err != nil {
+		return false, err
+	} else if !exist {
+		return false, fmt.Errorf("group id " + strconv.Itoa(groupID) + " not exist")
+	}
+	return exist, nil
+}
+
+func CheckGroupName(groupName string) (bool, error) {
+	query := `
+		SELECT EXISTS(SELECT group_id FROM group_info WHERE group_name = ?);
+	`
+	var exist bool
+	err := mariadb.DB.QueryRow(query, groupName).Scan(&exist)
+	if err != nil {
+		return false, err
+	}
+	return exist, nil
+}
+
+func CheckGroupNameForUpdate(groupID int, groupName string) (error) {
+	query := `
+		SELECT EXISTS(SELECT group_id FROM group_info WHERE group_name = ? AND group_id != ?);
+	`
+	var exist bool
+	err := mariadb.DB.QueryRow(query, groupName, groupID).Scan(&exist)
+	if err != nil {
+		return err
+	} else if exist {
+		return fmt.Errorf("group name " + groupName + " already exists")
+	}
+	return nil
+}
+
+func CheckAllGroupID(groups []int) error {
+	for _, id := range groups {
+		_, err := CheckGroupID(id)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func InGroup(groupID int, deviceID string) (bool, error) {
+	query := `
+		SELECT EXISTS(SELECT client_id FROM client_group WHERE group_id = ? AND client_id = ?);
+	`
+	var exist bool
+	err := mariadb.DB.QueryRow(query, groupID, deviceID).Scan(&exist)
+	if err != nil {
+		return false, err
+	}
+	return exist, nil
 }
