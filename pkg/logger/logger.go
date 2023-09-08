1370c9af75ccd7eb9220b2b91b67625241b47835
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-09-08 16:44:11 +0800

    disable log rotate

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -11,7 +11,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/imperfectgo/zap-syslog"
 	"github.com/imperfectgo/zap-syslog/syslog"
-	"gopkg.in/natefinch/lumberjack.v2"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -20,13 +19,8 @@ var Log *zap.Logger
 
 func InitLogger(path string, hostname string, app string) {
 	// file logger
-	// _, _ = os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	fileWriteSyncer := zapcore.AddSync(&lumberjack.Logger{
-		Filename:   path,
-		MaxSize:    500, // MB
-		MaxBackups: 3,
-		MaxAge:     28,   // days
-	})
+	file, _ := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	fileWriteSyncer := zapcore.AddSync(file)
 	productionCfg := zap.NewProductionEncoderConfig()
 	productionCfg.EncodeTime = zapcore.ISO8601TimeEncoder
 	fileEncoder := zapcore.NewJSONEncoder(productionCfg)
