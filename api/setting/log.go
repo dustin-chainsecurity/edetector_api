3b5bb357d3d117333a1eced585e95f1d63451416
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-09-18 17:45:52 +0800

    Added log API

--- api/setting/log.go
+++ api/setting/log.go
@@ -1,9 +1,20 @@
 package setting
 
 import (
+	"edetector_API/pkg/errhandler"
+	"edetector_API/pkg/mariadb/query"
+
 	"github.com/gin-gonic/gin"
 )
 
 func GetLogs(c *gin.Context) {
-
+	logs, err := query.LoadLogs()
+	if err != nil {
+		errhandler.Error(c, err, "Error retrieving logs")
+		return
+	}
+	c.JSON(200, gin.H{
+		"isSuccess": true,
+		"logs": logs,
+	})
 }
--- pkg/mariadb/query/log.go
+++ pkg/mariadb/query/log.go
@@ -1,10 +1,29 @@
 package query
 
+import "edetector_API/pkg/mariadb"
+
 type Log struct {
-	Id      int    `json:"id"`
-	Level   string `json:"level"`
-	Service string `json:"service"`
-	Message string `json:"content"`
+	Id        int    `json:"id"`
+	Level     string `json:"level"`
+	Service   string `json:"service"`
+	Content   string `json:"content"`
+	Timestamp string `json:"timestamp"`
 }
 
-
+func LoadLogs() ([]Log, error) {
+	query := "SELECT id, timestamp, level, service, content FROM log ORDER BY timestamp DESC"
+	rows, err := mariadb.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var logs []Log
+	for rows.Next() {
+		var log Log
+		if err := rows.Scan(&log.Id, &log.Timestamp, &log.Level, &log.Service, &log.Content); err != nil {
+			return nil, err
+		}
+		logs = append(logs, log)
+	}
+	return logs, nil
+}
