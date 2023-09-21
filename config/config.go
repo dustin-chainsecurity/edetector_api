97c017131ecf373efb6c0e94294523924d2df6ea
Dustin <dustin@chainsecurity.asia>
2023-09-21 12:00:48 +0800

    env path

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/spf13/viper"
@@ -12,18 +11,21 @@ var Viper *viper.Viper
 // Config is a struct that holds the configuration for the application
 func LoadConfig() *viper.Viper {
 	vp := viper.New()
-	vp.SetConfigFile("app.env")
+	vp.SetConfigFile("./config/app.env")
 	vp.SetConfigType("env")
-	vp.AutomaticEnv()
+	// vp.AutomaticEnv()
 	if err := vp.ReadInConfig(); err != nil {
-		fmt.Printf("Env file not found")
+		log.Printf("Env file not found")
+		// for _, env := range os.Environ() {
+		// 	log.Printf("ENV: %s", env)
+		// }
 	} else {
-		fmt.Println("Config file loaded successfully")
+		log.Println("Config file loaded successfully")
 	}
 	Viper = vp
-	log.Printf("Viper configuration:")
-	for _, key := range Viper.AllKeys() {
-		log.Printf("%s: %s", key, Viper.GetString(key))
-	}
+	// log.Printf("Viper configuration:")
+	// for _, key := range Viper.AllKeys() {
+	// 	log.Printf("%s: %s", key, Viper.GetString(key))
+	// }
 	return vp
 }
